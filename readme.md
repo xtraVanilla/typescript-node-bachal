6326e3a7c620f496af14259b4c547ffb56f1d437
Ashton Morris <amjsdev@gmail.com>
2022-04-06 09:48:22 -0400

    user controllers

--- readme.md
+++ readme.md
@@ -1,3 +1,8 @@
 npm
+<br />
 npm run package
+<br />
 npm run start
+
+<br />
+[ notes re: deficiencies w/ dynamoose library ]
--- src/users.ts
+++
@@ -1,6 +0,0 @@
-export default [
-  { id: 1, type: "librarian" },
-  { id: 2, type: "user" },
-  { id: 3, type: "user" },
-  { id: 4, type: "user" },
-];
